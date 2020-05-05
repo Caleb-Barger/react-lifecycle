c0ae2aac86f5974a938c5e773ccf37d2bdf8fbc8
caleb-barger <bargercj20@hotmail.com>
2020-05-05 10:10:39 -0700

    lifecycle method logs

--- src/App.js
+++ src/App.js
@@ -7,13 +7,32 @@ import Pokemon from './components/Pokemon';
 
 class App extends React.Component {
   constructor() {
+    console.log("Constructor")
     super();
     this.state = {
-      pokemon: data
+      pokemon: []
     };
   }
 
+  componentDidMount(){
+    console.log("Component has mounted")
+    this.setState({pokemon: data})
+
+    // window.addEventListener('resize', this.handleResize)
+  }
+
+  componentDidUpdate(){
+    console.log("Component has updated")
+  }
+
+  // componentWillUnmount(){
+  //   window.removeEventListener('resize', this.handleResize)
+  // }
+
+  // handleResize = () => {console.log("RESIZING THE WINDOW")}
+
   render() {
+    console.log("Component has rendered")
     return (
       <div className="App">
         <Pokemon pokemon={this.state.pokemon} />
